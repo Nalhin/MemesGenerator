1f3efd4a6a7c9a641350e1c7851d743cc1673d68
nalhin <krol.wow@gmail.com>
2020-07-04 23:44:33 +0200

    fix: app component test.

--- application/src/app/app.component.spec.ts
+++ application/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, LayoutModule],
       declarations: [AppComponent],
     }).compileComponents();
   }));
